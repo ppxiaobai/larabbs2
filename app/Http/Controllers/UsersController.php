639f54d0eeadcc1748b6b4eee6ce84e5b6e2d39d
ppxiaobai <309997474@qq.com>
2021-03-28 17:05:26 +0800

    裁剪头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,12 +24,12 @@ class UsersController extends Controller
     }
 
     // 编辑保存
-    public function update(UserRequest $request,ImageUploadHandler $uploader, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
         $data = $request->all();
 
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
